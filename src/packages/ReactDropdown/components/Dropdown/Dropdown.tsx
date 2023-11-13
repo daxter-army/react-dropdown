168173000975a0c5c45443851b3f30e4b5c2dd11
Mehul Singh Teya <mehulsaini456@gmail.com>
2023-11-13 23:42:12 +0530

    fix: scroll to the first element, when first keyboard nav happens, then mouse happens, then element was getting selected but, scrolling was not happening

--- src/packages/ReactDropdown/components/Dropdown/Dropdown.tsx
+++ src/packages/ReactDropdown/components/Dropdown/Dropdown.tsx
@@ -1,78 +1,82 @@
-import React, { createRef, forwardRef } from 'react'
+import { createRef, forwardRef } from "react";
 
 import S from "./Styles";
 
-import DropdownProps from "./Props"
+import DropdownProps from "./Props";
 
-const Dropdown = forwardRef<HTMLUListElement, DropdownProps>(({
-	list,
-	focusIndex,
-	activeIndex,
-	optionItemsRef,
-	focusIndexHandler,
-	onClickItemHandler,
-	isMouseHoverAllowed,
-	customDropdownStyles,
-	customDropdownItemStyles,
-	isMouseHoverAllowedHandler,
-}, ref) => {
+const Dropdown = forwardRef<HTMLUListElement, DropdownProps>(
+  (
+    {
+      list,
+      focusIndex,
+      activeIndex,
+      optionItemsRef,
+      focusIndexHandler,
+      onClickItemHandler,
+      isMouseHoverAllowed,
+      customDropdownStyles,
+      customDropdownItemStyles,
+      isMouseHoverAllowedHandler,
+    },
+    ref
+  ) => {
+    // Creating refs and assigning it to the elements
+    const itemRefHandler = () => {
+      if (optionItemsRef.current.length <= list.length) {
+        optionItemsRef.current.push(createRef<HTMLLIElement>());
+      }
+    };
 
-	// Creating refs and assigning it to the elements
-	const itemRefHandler = () => {
-		if (optionItemsRef.current.length <= list.length) {
-			optionItemsRef.current.push(createRef<HTMLLIElement>())
-		}
-	}
+    const onItemMouseEnterHandler = (i: number) => {
+      focusIndexHandler(i);
+    };
 
-	const onItemMouseEnterHandler = (i: number) => {
-		focusIndexHandler(i)
-	}
+    const onDropdownMouseLeaveHandler = () => {
+      focusIndexHandler(activeIndex);
+    };
 
-	const onDropdownMouseLeaveHandler = () => {
-		focusIndexHandler(-1)
-	}
+    const onDropdownMouseMoveHandler = () => {
+      if (!isMouseHoverAllowed) {
+        isMouseHoverAllowedHandler(true);
+      }
+    };
 
-	const onDropdownMouseMoveHandler = () => {
-		if (!isMouseHoverAllowed) {
-			isMouseHoverAllowedHandler(true)
-		}
-	}
+    const onClickDropdownHandler = () => {
+      if (!isMouseHoverAllowed) {
+        isMouseHoverAllowedHandler(true);
+      }
+    };
 
-	const onClickDropdownHandler = () => {
-		if (!isMouseHoverAllowed) {
-			isMouseHoverAllowedHandler(true)
-		}
-	}
+    return (
+      <S.Dropdown
+        ref={ref}
+        style={customDropdownStyles}
+        onClick={onClickDropdownHandler}
+        onMouseMove={onDropdownMouseMoveHandler}
+        onMouseLeave={onDropdownMouseLeaveHandler}
+      >
+        {list.map((item, i) => {
+          // Creating refs for every element
+          itemRefHandler();
 
-	return (
-		<S.Dropdown
-			ref={ref}
-			style={customDropdownStyles}
-			onClick={onClickDropdownHandler}
-			onMouseMove={onDropdownMouseMoveHandler}
-			onMouseLeave={onDropdownMouseLeaveHandler}
-		>
-			{
-				list.map((item, i) => {
-					// Creating refs for every element
-					itemRefHandler()
+          return (
+            <S.DropdownItem
+              key={item.id}
+              isFocus={focusIndex === i}
+              isActive={activeIndex === i}
+              isMouseOn={isMouseHoverAllowed}
+              ref={optionItemsRef.current[i]}
+              style={customDropdownItemStyles}
+              onClick={() => onClickItemHandler(item, i)}
+              onMouseEnter={() => onItemMouseEnterHandler(i)}
+            >
+              {item.label}
+            </S.DropdownItem>
+          );
+        })}
+      </S.Dropdown>
+    );
+  }
+);
 
-					return <S.DropdownItem
-						key={item.id}
-						isFocus={focusIndex === i}
-						isActive={activeIndex === i}
-						isMouseOn={isMouseHoverAllowed}
-						ref={optionItemsRef.current[i]}
-						style={customDropdownItemStyles}
-						onClick={() => onClickItemHandler(item, i)}
-						onMouseEnter={() => onItemMouseEnterHandler(i)}
-					>
-						{item.label}
-					</S.DropdownItem>
-				})
-			}
-		</S.Dropdown>
-	)
-})
-
-export default Dropdown
+export default Dropdown;
